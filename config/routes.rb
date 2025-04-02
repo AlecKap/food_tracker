87134d58e6e21d7b10f9ab83239d631e6c0263d0
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-04-02 14:35:48 -0600

    Set the root path to the welcome#index action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
   # root "posts#index"
+  root "welcome#index"
   resources :entry_logs
 end
