321f0e1ea2160d91bb296794b35cc3ef28f680c0
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-04-04 18:54:22 -0600

    Refactor routes to simplify entry logs management by removing nested resources under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
   devise_for :users
-  resources :users do
-    resources :entry_logs, controller: "users/entry_logs"
-  end
+  resources :entry_logs
 end
