6f278be4c38bcfbb286dfb4775d6dc48294645cc
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-04-05 22:18:48 -0600

    feat: Add validations for meal attributes to ensure data integrity

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,10 @@
 class Meal < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :calories, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 2000 }
+  validates :protein, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 500 }
+  validates :added_sugars, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 200 }
+  validates :total_sugars, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 200 }
+  validates :carbs, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 500 }
 end
