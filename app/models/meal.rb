6114f92c734a7ddd36fbddb96138eb97895d34b2
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-04-08 16:04:40 -0600

    fix: Add uniqueness validation to meal name to prevent duplicates

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,10 +1,12 @@
 class Meal < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :calories, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 2000 }
   validates :protein, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 500 }
   validates :added_sugars, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 200 }
   validates :total_sugars, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 200 }
   validates :carbs, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 500 }
+
+  
 end
